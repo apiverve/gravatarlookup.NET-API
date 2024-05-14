b6d22ec849855542d9d1307f19d20eed1ce0c5d0
APIVerve Team <hello@apiverve.com>
2024-05-14 15:27:09 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,106 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class photos
+{
+    [JsonProperty("value")]
+    public string value { get; set; }
+
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+}
+
+public class contactInfo
+{
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+    [JsonProperty("value")]
+    public string value { get; set; }
+
+}
+
+public class urls
+{
+    [JsonProperty("title")]
+    public string title { get; set; }
+
+    [JsonProperty("value")]
+    public string value { get; set; }
+
+}
+
+public class profileBackground
+{
+    [JsonProperty("opacity")]
+    public int opacity { get; set; }
+
+}
+
+public class shareflags
+{
+    [JsonProperty("search_engines")]
+    public bool searchengines { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("hash")]
+    public string hash { get; set; }
+
+    [JsonProperty("requestHash")]
+    public string requestHash { get; set; }
+
+    [JsonProperty("profileUrl")]
+    public string profileUrl { get; set; }
+
+    [JsonProperty("preferredUsername")]
+    public string preferredUsername { get; set; }
+
+    [JsonProperty("thumbnailUrl")]
+    public string thumbnailUrl { get; set; }
+
+    [JsonProperty("photos")]
+    public photos[] photos { get; set; }
+
+    [JsonProperty("last_profile_edit")]
+    public string lastprofileedit { get; set; }
+
+    [JsonProperty("displayName")]
+    public string displayName { get; set; }
+
+    [JsonProperty("company")]
+    public string company { get; set; }
+
+    [JsonProperty("contactInfo")]
+    public contactInfo[] contactInfo { get; set; }
+
+    [JsonProperty("urls")]
+    public urls[] urls { get; set; }
+
+    [JsonProperty("profileBackground")]
+    public profileBackground profileBackground { get; set; }
+
+    [JsonProperty("share_flags")]
+    public shareflags shareflags { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
